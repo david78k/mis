127c277c2b5caffbf34afe85afd92ef47f775cc5
david78k <root@c11node28.(none)>
2013-10-05 23:27:00 -0400

    hw3.m

---
+++ hw3.m
@@ -0,0 +1,21 @@
+fprintf ('Hello, world!\n');
+
+%B = load ('data.mat', '-ascii')
+
+A = [ 1:3; 4:6; 7:9 ];
+
+%E = [1 0 0; 1 1 0; 0 1 1]
+%inv(E)
+
+R = [1 3 0 5; 0 0 1 6; 0 0 0 0];
+%inv(R)
+
+%A = [0 1 2 3 4; 0 1 2 4 6; 0 0 0 1 2]
+%null(A)
+%null(A, 2)
+
+x1
+x2
+x3
+
+lambda
