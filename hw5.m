ea626fdd1d55db1b30a71a7710ce5451953443c6
david78k <root@c11node28.(none)>
2013-10-24 16:09:51 -0400

    hw5

--- hw5.m
+++ hw5.m
@@ -1,21 +1,15 @@
 % Minkowski inequality
-fprintf ('Hello, world!\n');
+x = rand(1, 10)
+%x = randn(1, 10)
+y = rand(1, 10)
+%y = randn(1, 10)
+ps = [1, 2, 10, 100, Inf]
 
-%B = load ('data.mat', '-ascii')
-
-A = [ 1:3; 4:6; 7:9 ];
-
-%E = [1 0 0; 1 1 0; 0 1 1]
-%inv(E)
-
-%A = [0 1 2 3 4; 0 1 2 4 6; 0 0 0 1 2]
-%null(A)
-%null(A, 2)
-
-% eigenvectors
-%x = [0 0 0]
-x = [0; 10]
-ps = {1, 2, 10, 100, infinity}
-
-% eigenvalues
-lambda = [0 0 0]
+for p = ps
+	p
+	norm(x+y,p)
+	norm(x,p) + norm(y,p)
+	if (p == Inf)
+		max(x) + max (y)
+	end
+end
