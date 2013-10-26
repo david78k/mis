a99c687b2905701fcce0f3af9d673cc81cd17cc6
david78k <root@c11node28.(none)>
2013-10-26 09:53:57 -0400

    hw5.m

--- hw5.m
+++ hw5.m
@@ -1,15 +1,26 @@
 % Minkowski inequality
-x = rand(1, 10)
+x = rand(1, 10);
 %x = randn(1, 10)
-y = rand(1, 10)
+y = rand(1, 10);
 %y = randn(1, 10)
-ps = [1, 2, 10, 100, Inf]
+ps = [1, 2, 10, 100, Inf];
+
+%format short
+format compact
+x
+y
+ps
 
 for p = ps
-	p
-	norm(x+y,p)
-	norm(x,p) + norm(y,p)
-	if (p == Inf)
-		max(x) + max (y)
+	norm_sum = norm(x+y,p);
+	norm_pq = norm(x,p) + norm(y,p);
+	comp = '<=';
+	if (norm_sum > norm_pq)
+		comp = '>';		
 	end
+
+	fprintf('p = %d: ||x + y|| = %f %s ||x| + ||y|| = %f\n', p, norm_sum, comp, norm_pq)	
+%	if (p == Inf)
+%		max(x) + max (y)
+%	end
 end
